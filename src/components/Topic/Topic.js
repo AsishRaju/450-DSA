1aa835f98e54b9a4244d86537c92170d6b372d47
Chetan <61468014+Unknown39825@users.noreply.github.com>
2021-07-05 08:13:23 +0530

    note.length undefined fix

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -60,19 +60,19 @@ export default function Topic({ data, updateData }) {
 							</a>
 							<OverlayTrigger
 								placement="left"
-								overlay={question.Notes.length !== 0 ? renderTooltipView : renderTooltipAdd}
+								overlay={(question.Notes&&question.Notes.length !== 0) ? renderTooltipView : renderTooltipAdd}
 							>
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
 									width="16"
 									height="16"
 									fill="currentColor"
-									class={question.Notes.length !== 0 ? "bi bi-sticky-fill" : "bi bi-sticky"}
+									class={(question.Notes&&question.Notes.length !== 0) ? "bi bi-sticky-fill" : "bi bi-sticky"}
 									viewBox="0 0 16 16"
 									style={{ float: "right", color: "green", cursor: "pointer" }}
 									onClick={() => shownotes(index)}
 								>
-									{question.Notes.length !== 0 ? (
+									{(question.Notes&&question.Notes.length !== 0) ? (
 										<path d="M2.5 1A1.5 1.5 0 0 0 1 2.5v11A1.5 1.5 0 0 0 2.5 15h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 15 8.586V2.5A1.5 1.5 0 0 0 13.5 1h-11zm6 8.5a1 1 0 0 1 1-1h4.396a.25.25 0 0 1 .177.427l-5.146 5.146a.25.25 0 0 1-.427-.177V9.5z" />
 									) : (
 										<path d="M2.5 1A1.5 1.5 0 0 0 1 2.5v11A1.5 1.5 0 0 0 2.5 15h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 15 8.586V2.5A1.5 1.5 0 0 0 13.5 1h-11zM2 2.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5V8H9.5A1.5 1.5 0 0 0 8 9.5V14H2.5a.5.5 0 0 1-.5-.5v-11zm7 11.293V9.5a.5.5 0 0 1 .5-.5h4.293L9 13.793z" />
