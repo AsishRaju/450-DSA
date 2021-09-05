a3d4fb8885317377757e199e55ea72c6adac9dc3
AsishRaju <41658324+AsishRaju@users.noreply.github.com>
2021-09-05 22:08:23 +0530

    made pick random button inline

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -13,6 +13,7 @@ import "react-toastify/dist/ReactToastify.min.css";
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import "./Topic.css";
 import { ThemeContext } from "../../App";
+import Button from "react-bootstrap/Button";
 
 export default function Topic({ data, updateData }) {
 	/*
@@ -27,7 +28,6 @@ export default function Topic({ data, updateData }) {
 	const [select, setSelected] = useState([]);
 	const [questionsTableData, setQuestionsTableData] = useState([]);
 	const [topicName, setTopicName] = useState("");
-	const [random, setRandom] = useState(0)
 
 	const dark = useContext(ThemeContext);
 	// updating states using useEffect with dependency  on `data` prop
@@ -120,6 +120,7 @@ export default function Topic({ data, updateData }) {
 						aria-describedby="basic-addon2"
 						onChange={handleChange}
 					/>
+					<InputGroup.Append>{RandomButton()}</InputGroup.Append>
 				</InputGroup>
 			</div>
 		);
@@ -221,7 +222,7 @@ export default function Topic({ data, updateData }) {
 	//Notes component
 	const NoteSection = (props) => {
 		let id = localStorage.getItem("cid");
-		
+
 		const [quickNotes, setQuickNotes] = useState(data.questions[id]?.Notes);
 		const addnewnotes = (event) => {
 			setQuickNotes(event.target.value);
@@ -287,24 +288,18 @@ export default function Topic({ data, updateData }) {
 		document.getElementsByClassName("question-title")[0].innerHTML = data.questions[ind].Problem;
 	}
 
-	function getRandom() {
-		let min=0;
-		let max=data.questions.length-1;
-		let rnd=Math.floor(Math.random() * (max - min)) + min;
-		setRandom(rnd) 
-	}
-	function RandomButton()
-	{
+	function RandomButton() {
+		let min = 0;
+		let max = data.questions.length - 1;
+		let rnd = Math.floor(Math.random() * (max - min)) + min;
 		return (
-		<a
-		href={data.questions[random].URL}
-		target="_blank"
-		style={{ fontWeight: "600" }}
-		className="question-link"
-		>
-		PICK A RANDOM QUESTION
-		</a>
-		)
+			<Button variant="outline-primary" href={data.questions[rnd].URL} target="_blank">
+				Pick Random{" "}
+				<span role="img" aria-label="woman-juggling-emoji">
+					🤹🏻‍♀️
+				</span>
+			</Button>
+		);
 	}
 	return (
 		<>
@@ -328,8 +323,7 @@ export default function Topic({ data, updateData }) {
 					{(props) => (
 						<div>
 							<div className="header-rand">
-							<SearchBar {...props.searchProps} />
-							<div className="random" onClick={getRandom}>{RandomButton()}</div>
+								<SearchBar {...props.searchProps} />
 							</div>
 							<div className="container container-custom" style={{ overflowAnchor: "none" }}>
 								<Fade duration={600}>
@@ -341,7 +335,7 @@ export default function Topic({ data, updateData }) {
 				</ToolkitProvider>
 			)}
 			<ToastContainer />
-			<NoteSection  />
+			<NoteSection />
 		</>
 	);
 }
